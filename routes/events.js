f52d17a130d273aec9b704ba609498e7bfb91c5d
Mauricio Cartagena <mc973220@gmail.com>
2020-11-03 02:32:40 -0400

    finalización del backend

--- routes/events.js
+++ routes/events.js
@@ -4,7 +4,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
-const validarCampos = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 
@@ -14,13 +15,30 @@ const router = Router();
 router.use( validarJWT );
 
 // Obtener evento
-router.get('/', actualizarEvento );
+router.get('/', getEventos );
 
 // Crear un nuevo evento
-router.post('/newEvents', crearEvento);
+router.post(
+    '/',
+    [
+        check('title','El titulo es obligatorio').not().isEmpty(),
+        check('start','Fecha de inicio es obligatoria').custom( isDate ),
+        check('end','Fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento 
+);
 
 // Actualizar evento
-router.put('/:id', actualizarEvento);
+router.put(
+    '/:id',
+    [
+        check('title','El titulo es obligatorio').not().isEmpty(),
+        check('start','Fecha de inicio es obligatoria').custom( isDate ),
+        check('end','Fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    actualizarEvento);
 
 // Eliminar evento
 router.delete('/:id', eliminarEvento);
