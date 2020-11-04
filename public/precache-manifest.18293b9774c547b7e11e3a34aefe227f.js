ac08e0f17449085daaa4386f0bddf46c4f66ae28
Mauricio Cartagena <mc973220@gmail.com>
2020-11-03 23:25:10 -0400

    v1 CalendarAPP

---
+++ public/precache-manifest.18293b9774c547b7e11e3a34aefe227f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9441de7cc0f87a0c05e00486c59028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "032de6163840b49d781c",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "1b6bbe7ad2a2ccbc8718",
+    "url": "/static/css/main.f07c2b6e.chunk.css"
+  },
+  {
+    "revision": "032de6163840b49d781c",
+    "url": "/static/js/2.0eafe5ac.chunk.js"
+  },
+  {
+    "revision": "62dc9784057de1bc85a8684fec455f1b",
+    "url": "/static/js/2.0eafe5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6bbe7ad2a2ccbc8718",
+    "url": "/static/js/main.f88d5ecd.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
